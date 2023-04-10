b8ed3462d2caa8546949230e5f2e3947d473505b
tbszlg <tobizillig@gmail.com>
2023-04-10 23:12:53 +0200

    Rough implementation of Rust onnx interface
    Signed-off-by: tbszlg <tobizillig@gmail.com>

---
+++ src/face_recognition.rs
@@ -0,0 +1,73 @@
+use eyre::Result;
+use image::{imageops::FilterType::Nearest, DynamicImage};
+use onnxruntime::{
+    environment::Environment,
+    ndarray::{Array1, Array3, Array4, Axis},
+    session::Session,
+    GraphOptimizationLevel,
+};
+use std::ops::Deref;
+
+const MODEL_PATH: &str = "assets/models/w600k_mbf.onnx";
+
+
+/// Initialize the onnxruntime inference environment
+pub fn initialize_environment() -> Result<Environment> {
+    let env = Environment::builder()
+        .with_name("onnxruntime-rs")
+        .with_log_level(onnxruntime::LoggingLevel::Verbose)
+        .build()?;
+    Ok(env)
+}
+
+
+/// Initialize inference session using `env` and specified model
+pub fn initialize_session(env: &Environment) -> Result<Session> {
+    let session = env
+        .new_session_builder()?
+        .with_optimization_level(GraphOptimizationLevel::Basic)?
+        .with_number_threads(1)?
+        .with_model_from_file(MODEL_PATH)?;
+    Ok(session)
+}
+
+
+/// Preprocess image input. Resize (?, ?, 3) -> (1, 3, input_shape[0], input_shape[1]) and
+/// normalize pixel values to [-1, 1]
+pub fn preprocess_input(image: DynamicImage, session: &Session) -> Result<Vec<Array4<f32>>> {
+    let input_dimensions = &session.inputs[0].dimensions;
+    let output_dimensions = &session.outputs[0].dimensions;
+
+    let image_resized = image
+        .resize_exact(input_dimensions[2].unwrap(), input_dimensions[3].unwrap(), Nearest)
+        .to_rgb8();
+    let height = image_resized.height();
+    let width = image_resized.width();
+    let image_resized = Array3::from_shape_fn((height as usize, width as usize, 3 as usize), |(i, j, k)| {
+        image_resized[(j as u32, i as u32)][k]
+    })
+        .permuted_axes([2, 0, 1]);
+
+    let input_image = image_resized
+        .insert_axis(Axis(0))
+        .map(|v| *v as f32 / 127.5 - 1.0);
+    let input_tensor = vec![input_image];
+    Ok(input_tensor)
+}
+
+pub fn run_inference(input_tensor: Vec<Array4<f32>>, session: &mut Session) -> Result<Array1<f32>> {
+    let output_tensor = session
+        .run(input_tensor)?[0]
+        .deref()
+        .to_owned();
+    let output = Array1::<f32>::from_iter(output_tensor.iter().cloned());
+    let norm = output.dot(&output).sqrt();
+    let output = output / norm;
+    Ok(output)
+}
+
+
+pub fn calculate_similarity(embedding1: &Array1<f32>, embedding2: &Array1<f32>) -> Result<f32> {
+    let similarity = embedding1.dot(embedding2);
+    Ok(similarity)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
+use image::open;
 use pyo3::prelude::*;
+
+mod face_recognition;
+
+const IMAGE_PATH_TOM: &str = "assets/tom.png";
+const IMAGE_PATH_HANKS: &str = "assets/hanks.jpg";
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -6,9 +12,26 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn get_face_embeddings() -> PyResult<f32> {
+    let env = face_recognition::initialize_environment().unwrap();
+    let mut session = face_recognition::initialize_session(&env).unwrap();
+    let image = open(IMAGE_PATH_TOM).unwrap();
+    let input = face_recognition::preprocess_input(image, &session).unwrap();
+    let output_1 = face_recognition::run_inference(input, &mut session).unwrap();
+
+    let image = open(IMAGE_PATH_HANKS).unwrap();
+    let input = face_recognition::preprocess_input(image, &session).unwrap();
+    let output_2 = face_recognition::run_inference(input, &mut session).unwrap();
+
+    let similarity = face_recognition::calculate_similarity(&output_1, &output_2).unwrap();
+    Ok(similarity)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn oxidized_python(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(get_face_embeddings, m)?)?;
     Ok(())
 }
